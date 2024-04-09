c858049efe1d771a89338903baa215083f2ce2bd
nameyouxuan <nameyouxuan@gmail.com>
2024-04-09 23:22:19 +0800

    20240409

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n: i32) -> i32 {
+    if n <= 40 {
+        n * 2
+    }
+    else {
+        n
+    }
+}
 
 // Don't modify this function!
 #[test]
